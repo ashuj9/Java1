3fda28dca525bab8da0e105a39db0fb5825c8630
ashuj9 <138884875+ashuj9@users.noreply.github.com>
2023-07-08 12:49:13 +0530

    First Commit
    Added a File

---
+++ GitHub/src/Javava/HelloWOrld.java
@@ -0,0 +1,8 @@
+package Javava;
+
+public class HelloWOrld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ GitHub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author guggi
+ *
+ */
+module GitHub {
+}
